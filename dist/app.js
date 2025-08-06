b11599af126fad47e13c5629da6eb0e584646133
Manikanta <manikanta@electrumcharging.com>
2025-08-06 18:53:46 +0530

    initial commit

--- dist/app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const http = require('http');
-const server = http.createServer((req, res) => {
-    res.end("Hello");
-});
-server.listen(6000, () => {
-    console.log("Server is running ");
-});
